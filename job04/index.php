502b1f20d8b1dff8319a49fc66017e4f7f400c46
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-05-04 13:48:37 +0200

    Add files via upload
    job 4 en cours, problème avec l'affichage du contenu du bouton hamburger

---
+++ job04/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header >
+    <nav class="lien" >
+        <a href="">Acceuil</a>
+        <a href="">Présentation</a>
+        <a href="">Contact</a>
+    </nav>
+    <nav class="menu">
+        <div class="burger" type="checkbox"></div>
+    </nav>
+</header>
+
+
+    
+    <main>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </main>
+
+</body>
+</html>
