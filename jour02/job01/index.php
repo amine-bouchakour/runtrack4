833c6763fc5f0fab6078a40c01505817ee6f5e8b
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-05-05 17:09:56 +0200

    Add files via upload
    job1 terminé

---
+++ jour02/job01/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <nav></nav>
+    </header>
+
+    <form action="">
+
+        <div>
+            <label for="M">M.</label>
+            <input type="radio" id="M" name="civilité" value="M"><br>
+        </div>
+        <div>
+            <label for="Mme">Mme</label>
+            <input type="radio" id="Mme" name="civilité" value="Mme"><br>
+        </div>
+        <div>
+            <label for="Mlle">Mlle</label>
+            <input type="radio" id="Mlle" name="civilité" value="Mlle"><br><br>
+        </div>
+
+        <input type="text" name="" placeholder="Nom"><br>
+        <input type="text" name="" placeholder="Prenom"><br>
+        <input type="text" name="" placeholder="Adresse"><br>
+        <input type="email" name="" placeholder="Email"><br>
+        <input type="password" name="" placeholder="Password"><br>
+        <input type="password" name="" placeholder="Confirm Pawword"><br><br>
+
+        <input type="checkbox" name="Voyages" placeholder="">
+        <label for="Voyages">Voyages</label><br>
+        <input type="checkbox" name="Informatique" placeholder="">
+        <label for="Informatique">Informatique</label><br>
+        <input type="checkbox" name="Sport" placeholder="">
+        <label for="Sport">Sport</label><br>
+        <input type="checkbox" name="Lecture" placeholder="">
+        <label for="Lecture">Lecture</label><br><br>
+        <input type="submit" value="Valider"><br><br>
+    </form>
+
+
+    <footer>
+        <a href="index.php">Acceuil</a>
+        <a href="index.php">Inscription</a>
+        <a href="index.php">Connexion</a>
+        <a href="index.php">Rechercher</a>
+    </footer>
+</body>
+
+</html>
