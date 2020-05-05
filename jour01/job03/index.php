af990d79487f94a9dc57cdf366b38edb4f457126
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-05-05 17:11:19 +0200

    Add files via upload
    ajout dossier jour 1 pour les job jour01

---
+++ jour01/job03/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header media="screen and min-width:767px">
+    <a href="">Acceuil</a>
+    <a href="">Présentation</a>
+    <a href="">Contact</a>
+</header>
+
+<header media="screen and max-width:767px">
+    <div class="menu">
+        <input class="burger" type="checkbox">
+        <!-- <span class="title">Sitename</span> -->
+    </div>
+</header>
+    
+    <main>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </main>
+
+</body>
+</html>
---
+++ jour01/job04/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header >
+    <nav class="lien">
+        <div><a href="">Acceuil</a></div>
+        <div><a href="">Présentation</a></div>
+        <div><a href="">Contact</a></div>
+    </nav>
+    <nav class="menu">
+        <div class="burger" type="checkbox"></div>
+    </nav>
+</header>
+
+
+    
+    <main>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </main>
+
+</body>
+</html>
