a03016f8d80b66691d3544078e4990686d093169
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-05-05 21:25:23 +0200

    Add files via upload
    job5 terminé

---
+++ job05/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <div class="row card-panel orange lighten-3 center z-depth-4">
+            <img src="Panda3D.png" class="col s1" alt=""><br><br>
+            <a href="index.php" class="col s3 center black-text">A propos</a>
+            <a href="index.php" class="col s2 center black-text">Articles</a>
+            <a href="index.php" class="col s3 center black-text">Connexion</a>
+            <a href="index.php" class="col s2 center black-text">Inscription</a>
+        </div>
+    </header>
+
+    <div class="row center">
+
+        <div class="col s12 card-panel amber lighten-5 z-depth-4"><img src="pics1.jpg" alt=""> Un renard faisant la sieste ...</div>
+        <div class="col s6 offset-s3 card-panel amber lighten-5 z-depth-4" ><img src="pics4.jpeg" alt=""><br> Un animal mystérieux !</div>
+        <div class="col s8 offset-s2 card-panel amber lighten-5 z-depth-4"><img src="pics3.jpg" alt=""><br> Sans faire de bruit, il se rapproche...</div>
+        <div class="col s7 card-panel amber lighten-5 z-depth-4"><img src="pics6.jpg" alt=""> Réveillé, il apperçoit Tiny</div>
+        <div class="col s4 right card-panel amber lighten-5 z-depth-4"><img src="pics5.jpg" alt="">Tiny, regardant par la fenêtre ...</div>
+
+    
+    </div>
+
+
+    <footer class="card-panel orange lighten-2 z-depth-4">
+
+        <div class="row container">
+
+            <div class="col s3 center">
+                <a href="index.php" class="white-text lighten-5">A propos</a>
+            </div>
+            <div class="col s3 center">
+                <a href="index.php" class="white-text lighten-5">Articles</a>
+            </div>
+            <div class="col s3 center">
+                <a href="index.php" class="white-text lighten-5">Connexion</a>
+            </div>
+            <div class="col s3 center">
+                <a href="index.php" class="white-text lighten-5">Rechercher</a>
+            </div>
+
+        </div>
+    </footer>
+
+
+
+
+
+
+</body>
+
+</html>
